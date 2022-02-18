c64728eac32cfaea75f32d5314dbc6ee2fc1d13d
hudsonbay <manuelmenendezalfonso@gmail.com>
2022-02-18 18:04:54 -0500

    intial commit

---
+++ lib/plug_cowboy_playground.ex
@@ -0,0 +1,18 @@
+defmodule PlugCowboyPlayground do
+  @moduledoc """
+  Documentation for `PlugCowboyPlayground`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugCowboyPlayground.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/plug_cowboy_playground/application.ex
@@ -0,0 +1,20 @@
+defmodule PlugCowboyPlayground.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: PlugCowboyPlayground.Worker.start_link(arg)
+      # {PlugCowboyPlayground.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: PlugCowboyPlayground.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
