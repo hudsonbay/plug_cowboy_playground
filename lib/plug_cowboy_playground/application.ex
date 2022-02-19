b52ca853dc77bd81b10c076bb6ba52fae97dc086
hudsonbay <manuelmenendezalfonso@gmail.com>
2022-02-18 22:59:06 -0500

    feat: :sparkles: supervised the Cowboy web server when the app starts up

--- lib/plug_cowboy_playground/application.ex
+++ lib/plug_cowboy_playground/application.ex
@@ -4,17 +4,21 @@ defmodule PlugCowboyPlayground.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: PlugCowboyPlayground.Worker.start_link(arg)
-      # {PlugCowboyPlayground.Worker, arg}
+      {Plug.Cowboy,
+       scheme: :http, plug: PlugCowboyPlayground.Plug.HelloWorldPlug, options: [port: 8080]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: PlugCowboyPlayground.Supervisor]
+
+    Logger.info("Starting application...")
+
     Supervisor.start_link(children, opts)
   end
 end
