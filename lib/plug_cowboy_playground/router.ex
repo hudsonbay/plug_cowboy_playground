b80598a76c7576b59f8eef9580d34aa7733f4f79
hudsonbay <manuelmenendezalfonso@gmail.com>
2022-02-19 01:29:30 -0500

    feat: :sparkles: called `VerifyRequest` plug in our router
    With this code, we are telling our application to send incoming requests through the VerifyRequest plug before running through the code in the router

--- lib/plug_cowboy_playground/router.ex
+++ lib/plug_cowboy_playground/router.ex
@@ -1,11 +1,19 @@
 defmodule PlugCowboyPlayground.Router do
   use Plug.Router
 
+  alias PlugCowboyPlayground.Plug.VerifyRequest
+
+  plug(Plug.Parsers, parsers: [:urlencoded, :multipart])
+  plug(VerifyRequest, fields: ["content", "mimetype"], paths: ["/upload"])
   plug(:match)
   plug(:dispatch)
 
   get "/" do
     send_resp(conn, 200, "Welcome")
+  end
+
+  get "/upload" do
+    send_resp(conn, 201, "Uploaded")
   end
 
   match _ do
